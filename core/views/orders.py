33cfed97818c4e0182c1a1540782588a697ced4b
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-09-10 01:31:58 +0600

    Done

--- core/views/orders.py
+++ core/views/orders.py
@@ -5,8 +5,12 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.views import View
 from core.models.product import Product
 from core.models.orders import Order
+from core.middlewares.auth import auth_middleware
+from django.utils.decorators import method_decorator
+
 
 class OrderView(View):
+    @method_decorator(auth_middleware)
     def get(self,request):
         customer=request.session.get('customer')
         orders=Order.get_orders_by_customer(customer)
